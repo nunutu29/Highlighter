03f98b6a16e76c955e6f2dca9bdcf957c7dee6f2
Johnny <ionursachi.29@gmail.com>
2016-02-26 00:50:40 +0100

    Bug Fix

--- js/script.js
+++ js/script.js
@@ -42,28 +42,34 @@ Highlighter.prototype.byID = function(id, start, end){
 
 Highlighter.prototype.WrapNode = function(parentNode){
 	var len = parentNode.childNodes.length;
-	var childNodes = parentNode.childNodes;
+	//clone all childnodes
+	var childNodes = [];
+	for(var i = 0; i < len; i++)
+		childNodes.push(parentNode.childNodes[i]);
 	for(var i = 0; i < len; i++){
 		var child = childNodes[i];
 		if(child.nodeType == 3){
 			var text = child.nodeValue;
 			if(this.COUNT >= this.END) continue;
-			if((this.COUNT + text.length) <= this.START || text.trim() == ""){
+			if((this.COUNT + text.length) <= this.START){
 				this.COUNT += text.length;
 				continue;
 			}
 			
-			var startWrap = 0;
-			var endWrap = -1;
+			var startWrap = 0, endWrap = 0;
 			
 			if(this.COUNT < this.START)
 				startWrap = this.START - this.COUNT;
 			
-			if((this.COUNT + text.length) > this.END)
-				endWrap += this.END - this.COUNT;
-			
+			if((this.COUNT + text.length) >= this.END)
+				endWrap = this.END - this.COUNT;
+			else
+				if(startWrap == 0)
+					endWrap = text.length;
 			if(parentNode.nodeType == 1 && parentNode.className.indexOf(this.highlighted) != -1)
-			{
+			{	
+				//wrapper is the main element that we are searching
+				var wrapper = null;
 				if(startWrap > 0){
 					var wrapLeft = document.createElement("SPAN");
 					wrapLeft.className = parentNode.className;
@@ -71,32 +77,43 @@ Highlighter.prototype.WrapNode = function(parentNode){
 					wrapLeft.style.background = parentNode.style.background;
 					parentNode.replaceChild(wrapLeft, child);
 				}
-				if(endWrap != -1){
+				if(endWrap > 0){
 					var wrapRight = document.createElement("SPAN");
 					wrapRight.className = parentNode.className;
 					wrapRight.style.background = parentNode.style.background;
 					wrapRight.appendChild(document.createTextNode(text.slice(endWrap)));
 					parentNode.replaceChild(wrapRight, child);
-					parentNode.insertBefore(document.createTextNode(text.slice(startWrap, endWrap)), wrapRight);
+					
+					wrapper = document.createElement("SPAN");
+					wrapper.className = parentNode.className;
+					wrapper.style.background = parentNode.style.background;
+					wrapper.appendChild(document.createTextNode(text.slice(startWrap, endWrap)));
+					
+					parentNode.insertBefore(wrapper, wrapRight);
 				}
-				else if(startWrap > 0)
-					parentNode.appendChild(document.createTextNode(text.slice(startWrap)));
-			
-				if(parentNode.className.indexOf(this.CLASS[this.cssIndex]) == -1)
-					parentNode.className += " " + this.CLASS[this.cssIndex];
-				if(parentNode.className.indexOf(this.overLap) == -1){
-					parentNode.className += this.overLap;
-					parentNode.style.background = "repeating-linear-gradient(45deg, " + parentNode.style.background + ", " + parentNode.style.background + "5px, " + this.BACKGROUNDS[this.cssIndex] + " 5px," + this.BACKGROUNDS[this.cssIndex] + " 10px)";
+				else if(startWrap > 0){
+					wrapper = document.createElement("SPAN");
+					wrapper.className = parentNode.className;
+					wrapper.style.background = parentNode.style.background;
+					wrapper.appendChild(document.createTextNode(text.slice(startWrap)));
+					parentNode.appendChild(wrapper);
 				}
-				else
-				{
-					//getting last pixel declared
-					var Gradient = parentNode.style.background.substring(parentNode.style.background.lastIndexOf(" ") + 1, parentNode.style.background.lastIndexOf("px"));
-					//parsing it to integer, adding 5 and then back to string
-					var newGradient = (parseInt(Gradient) + 5).toString();
-					parentNode.style.background = parentNode.style.background.substring(0, parentNode.style.background.length - 1) + ", " + this.BACKGROUNDS[this.cssIndex] + " " + Gradient + "px, " + this.BACKGROUNDS[this.cssIndex] + " " + newGradient + "px)";
+				if(wrapper != null){
+					if(wrapper.className.indexOf(this.CLASS[this.cssIndex]) == -1)
+						wrapper.className += " " + this.CLASS[this.cssIndex];
+					if(wrapper.className.indexOf(this.overLap) == -1){
+						wrapper.className += this.overLap;
+						wrapper.style.background = "repeating-linear-gradient(45deg, " + wrapper.style.background + ", " + wrapper.style.background + "5px, " + this.BACKGROUNDS[this.cssIndex] + " 5px," + this.BACKGROUNDS[this.cssIndex] + " 10px)";
+					}
+					else
+					{
+						//getting last pixel declared
+						var Gradient = wrapper.style.background.substring(wrapper.style.background.lastIndexOf(" ") + 1, wrapper.style.background.lastIndexOf("px"));
+						//parsing it to integer, adding 5 and then back to string
+						var newGradient = (parseInt(Gradient) + 5).toString();
+						wrapper.style.background = wrapper.style.background.substring(0, wrapper.style.background.length - 1) + ", " + this.BACKGROUNDS[this.cssIndex] + " " + Gradient + "px, " + this.BACKGROUNDS[this.cssIndex] + " " + newGradient + "px)";
+					}
 				}
-				
 			}
 			else
 			{
